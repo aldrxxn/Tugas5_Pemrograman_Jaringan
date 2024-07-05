90d0b1c4711658882478f0e692b24c52780bdbc9
Andrian Tambunan <andriantambunan2752@gmail.com>
2024-07-05 20:26:22 +0700

    add:files

---
+++ runserver_pool.sh
@@ -0,0 +1,6 @@
+#jalankan 5 pool_server
+
+python3 server_process_pool_http.py 9032 &
+python3 server_process_pool_http.py 9033 &
+python3 server_process_pool_http.py 9034 &
+python3 server_process_pool_http.py 9035 &
